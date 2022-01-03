9e0730ff010e0e40d98896166ce6f84827a5734d
RhysH0w <93980599+RhysH0w@users.noreply.github.com>
2022-01-03 16:43:46 +0000

    Update solar system.py

--- solar system.py
+++ solar system.py
@@ -1,5 +1,6 @@
 # Import the librarys
 import pygame
+import random
 from planetsClass import *
 
 # Initialise the engines
@@ -16,39 +17,39 @@ turquoise = (97, 231, 199)
 
 size = (1200, 795)
 
-Sun = Planet(name='Sun', radius=50, colour=sun, speed=200, dfs=0, mass='1.989 × 10^30', angle = 1.5708, size = size)
+Sun = Planet(name='Sun', radius=50, colour=sun, speed=200, dfs=0, mass='1.989 × 10^30', angle = random.uniform(0,6.2832), size = size)
 # original = 856.26
 
 
-Mercury = Planet(name='Mercury', radius=3, colour=mercury, speed=0.00477, dfs=60, mass='3.285 × 10^23', angle = 1.5708, size = size)
+Mercury = Planet(name='Mercury', radius=3, colour=mercury, speed=0.00477, dfs=60, mass='3.285 × 10^23', angle = random.uniform(0,6.2832), size = size)
 MerDis = Mercury.get_attr("dfs") + Mercury.get_attr("radius") + Sun.get_attr("radius")
 # Original = 3
 
-Venus = Planet(name='Venus', radius=7, colour=venus, speed=0.00354, dfs=80, mass='4.867 × 10^24', angle = 1.5708, size = size)
+Venus = Planet(name='Venus', radius=7, colour=venus, speed=0.00354, dfs=80, mass='4.867 × 10^24', angle = random.uniform(0,6.2832), size = size)
 VenDis = Venus.get_attr("dfs") + Venus.get_attr("radius") + Sun.get_attr("radius")
 # Original = 7.44
 
-Earth = Planet(name='Earth', radius=8, colour=earth, speed=0.003, dfs=100, mass='5.9722 × 10^24', angle = 1.5708, size = size)
+Earth = Planet(name='Earth', radius=8, colour=earth, speed=0.003, dfs=100, mass='5.9722 × 10^24', angle = random.uniform(0,6.2832), size = size)
 EarDis = Earth.get_attr("dfs") + Earth.get_attr("radius") + Sun.get_attr("radius")
 # original = 7.83
 
-Mars = Planet(name='Mars', radius=4, colour=mars, speed=0.002424, dfs=120, mass='6.39 × 10^23', angle = 1.5708, size = size)
+Mars = Planet(name='Mars', radius=4, colour=mars, speed=0.002424, dfs=120, mass='6.39 × 10^23', angle = random.uniform(0,6.2832), size = size)
 MarDis = Mars.get_attr("dfs") + Mars.get_attr("radius") + Sun.get_attr("radius")
 # Original = 4.17
 
-Jupiter = Planet(name='Jupiter', radius=35, colour=jupiter, speed=0.001317, dfs=183, mass='1.898 × 10^27', angle = 1.5708, size = size)
+Jupiter = Planet(name='Jupiter', radius=35, colour=jupiter, speed=0.001317, dfs=183, mass='1.898 × 10^27', angle = random.uniform(0,6.2832), size = size)
 JupDis = Jupiter.get_attr("dfs") + Jupiter.get_attr("radius") + Sun.get_attr("radius")
 # Original = 85.97
 
-Saturn = Planet(name='Saturn', radius=30, colour=saturn, speed=0.000975, dfs=255, mass='5.683 × 10^26', angle = 1.5708, size = size)
+Saturn = Planet(name='Saturn', radius=30, colour=saturn, speed=0.000975, dfs=255, mass='5.683 × 10^26', angle = random.uniform(0,6.2832), size = size)
 SatDis = Saturn.get_attr("dfs") + Saturn.get_attr("radius") + Sun.get_attr("radius")
 # original = 71.61
 
-Uranus = Planet(name='Uranus', radius=25, colour=uranus, speed=0.000684, dfs=315, mass='8.681 × 10^25', angle = 1.5708, size = size)
+Uranus = Planet(name='Uranus', radius=25, colour=uranus, speed=0.000684, dfs=315, mass='8.681 × 10^25', angle = random.uniform(0,6.2832), size = size)
 UraDis = Uranus.get_attr("dfs") + Uranus.get_attr("radius") + Sun.get_attr("radius")
 # Original = 31.19
 
-Neptune = Planet(name='Neptune', radius=24, colour=neptune, speed=0.000546, dfs=370, mass='1.024 × 10^26', angle = 1.5708, size = size)
+Neptune = Planet(name='Neptune', radius=24, colour=neptune, speed=0.000546, dfs=370, mass='1.024 × 10^26', angle = random.uniform(0,6.2832), size = size)
 NepDis = Neptune.get_attr("dfs") + Neptune.get_attr("radius") + Sun.get_attr("radius")
 # Create the clock
 clock = pygame.time.Clock()
@@ -154,7 +155,7 @@ def start():
         # Game logic
 
         # Select font
-        titleFont = pygame.font.SysFont('Calibri', 25, True, False)
+        titleFont = pygame.font.SysFont('Calibri', 40, True, False)
         buttonFont = pygame.font.SysFont('Calibri', 25, True, False)
         title = titleFont.render("Welcome To The Solar System", True, pink)
         play = buttonFont.render("Play", True, turquoise)
@@ -162,10 +163,10 @@ def start():
 
         # Drawing code here
         gameDisplay.fill(black)
-        pygame.draw.rect(gameDisplay, white, [100, 530, 90, 55])
+        pygame.draw.rect(gameDisplay, white, [200, 530, 90, 55])
         pygame.draw.rect(gameDisplay, white, [370, 255, 130, 45])
-        gameDisplay.blit(title, [100, 50])
-        gameDisplay.blit(play, [120, 547])
+        gameDisplay.blit(title, [350, 100])
+        gameDisplay.blit(play, [220, 547])
         gameDisplay.blit(instruct, [380, 270])
         pygame.display.flip()
 
