38d577632e3ed6c580d714257a0fae35c795583e
RhysH0w <93980599+RhysH0w@users.noreply.github.com>
2022-01-11 11:02:23 +0000

    Update solar system.py

--- solar system.py
+++ solar system.py
@@ -23,43 +23,52 @@ Sun = Planet(name='Sun', radius=50, colour=sun, speed=200, dfs=0, mass='1.989 ×
 
 
 Mercury = Planet(name='Mercury', radius=3, colour=mercury, speed=0.00477, dfs=60, mass='3.285 × 10^23', angle = random.uniform(0,6.2832), size = size)
-MerDis = Mercury.get_attr("dfs") + Mercury.get_attr("radius") + Sun.get_attr("radius")
-# Original = 3
 
 Venus = Planet(name='Venus', radius=7, colour=venus, speed=0.00354, dfs=80, mass='4.867 × 10^24', angle = random.uniform(0,6.2832), size = size)
-VenDis = Venus.get_attr("dfs") + Venus.get_attr("radius") + Sun.get_attr("radius")
-# Original = 7.44
 
 Earth = Planet(name='Earth', radius=8, colour=earth, speed=0.003, dfs=100, mass='5.9722 × 10^24', angle = random.uniform(0,6.2832), size = size)
-EarDis = Earth.get_attr("dfs") + Earth.get_attr("radius") + Sun.get_attr("radius")
-# original = 7.83
 
 Mars = Planet(name='Mars', radius=4, colour=mars, speed=0.002424, dfs=120, mass='6.39 × 10^23', angle = random.uniform(0,6.2832), size = size)
-MarDis = Mars.get_attr("dfs") + Mars.get_attr("radius") + Sun.get_attr("radius")
-# Original = 4.17
 
 Jupiter = Planet(name='Jupiter', radius=35, colour=jupiter, speed=0.001317, dfs=183, mass='1.898 × 10^27', angle = random.uniform(0,6.2832), size = size)
-JupDis = Jupiter.get_attr("dfs") + Jupiter.get_attr("radius") + Sun.get_attr("radius")
-# Original = 85.97
 
 Saturn = Planet(name='Saturn', radius=30, colour=saturn, speed=0.000975, dfs=255, mass='5.683 × 10^26', angle = random.uniform(0,6.2832), size = size)
-SatDis = Saturn.get_attr("dfs") + Saturn.get_attr("radius") + Sun.get_attr("radius")
-# original = 71.61
 
 Uranus = Planet(name='Uranus', radius=25, colour=uranus, speed=0.000684, dfs=315, mass='8.681 × 10^25', angle = random.uniform(0,6.2832), size = size)
-UraDis = Uranus.get_attr("dfs") + Uranus.get_attr("radius") + Sun.get_attr("radius")
-# Original = 31.19
 
 Neptune = Planet(name='Neptune', radius=24, colour=neptune, speed=0.000546, dfs=370, mass='1.024 × 10^26', angle = random.uniform(0,6.2832), size = size)
-NepDis = Neptune.get_attr("dfs") + Neptune.get_attr("radius") + Sun.get_attr("radius")
+
 # Create the clock
 clock = pygame.time.Clock()
 
-planets = (Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune)
+planets = (Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune)
 
 gameDisplay = pygame.display.set_mode(size)
 pygame.display.set_caption("Solar System")
 
+
+def factPage(Title, mass, size, fact1,fact2):
+    end = False
+    while not end:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            mouse = pygame.mouse.get_pos()
+            print(mouse)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+
+        infoFont = pygame.font.Font("Slabo.ttf", 30)
+
+
+
+        gameDisplay.fill(black)
+
+
+        pygame.display.flip()
 
 def instrucions():
     end = False
@@ -119,7 +128,22 @@ def play1():
                 Planet.slowDown(planets)
 
             if event.type == pygame.MOUSEBUTTONDOWN and 40 < mouse[0] < 220 and 210 < mouse[1] < 270:
-                Planet.Start(planets)
+                Planet.Stop(planets)
+
+            for planet in planets:
+                ax = planet.get_centrex()
+                ay = planet.get_centrey()
+                radius1 = planet.get_attr("radius")
+                if event.type == pygame.MOUSEBUTTONDOWN and ax - radius1 < mouse[0] < ax + radius1 and ay - radius1 < mouse[1] < ay + radius1:
+
+                    title = planet.get_attr("name1")
+                    mass = planet.get_attr("mass")
+                    scale = planet.get_attr("size")
+                    fact1 = planet.get_attr("fact1")
+                    fact2 = planet.get_attr("fact2")
+
+                    factPage(title, mass, scale, fact1, fact2)
+
 
         # selecting the Fonts
         buttonFont = pygame.font.SysFont('Calibri', 25, True, False)
@@ -146,7 +170,7 @@ def play1():
         if 40 < mouse[0] < 220 and 70 < mouse[1] < 130:
             speedMore = optFont.render("+ Speed", True, mercury)
             speedLess = optFont.render("- Speed", True, white)
-            Start = optFont.render("Start", True, white)
+            Start = optFont.render("Stop", True, white)
             gameDisplay.blit(speedMore, [40, 70])
             gameDisplay.blit(speedLess, [48, 140])
             gameDisplay.blit(Start, [75, 210])
@@ -154,7 +178,7 @@ def play1():
         elif 40 < mouse[0] < 220 and 140 < mouse[1] < 200:
             speedMore = optFont.render("+ Speed", True, white)
             speedLess = optFont.render("- Speed", True, mercury)
-            Start = optFont.render("Start", True, white)
+            Start = optFont.render("Stop", True, white)
             gameDisplay.blit(speedMore, [40, 70])
             gameDisplay.blit(speedLess, [48, 140])
             gameDisplay.blit(Start, [75, 210])
@@ -162,7 +186,7 @@ def play1():
         elif 40 < mouse[0] < 220 and 210 < mouse[1] < 270:
             speedMore = optFont.render("+ Speed", True, white)
             speedLess = optFont.render("- Speed", True, white)
-            Start = optFont.render("Start", True, mercury)
+            Start = optFont.render("Stop", True, mercury)
             gameDisplay.blit(speedMore, [40, 70])
             gameDisplay.blit(speedLess, [48, 140])
             gameDisplay.blit(Start, [75, 210])
@@ -170,10 +194,20 @@ def play1():
         else:
             speedMore = optFont.render("+ Speed", True, white)
             speedLess = optFont.render("- Speed", True, white)
-            Start = optFont.render("Start", True, white)
+            Start = optFont.render("Stop", True, white)
             gameDisplay.blit(speedMore, [40, 70])
             gameDisplay.blit(speedLess, [48, 140])
             gameDisplay.blit(Start, [75, 210])
+
+        for planet in planets:
+            ax = planet.get_centrex()
+            ay = planet.get_centrey()
+            radius1 = planet.get_attr("radius")
+            name1 = planet.get_attr("name")
+            if ax - radius1 < mouse[0] < ax + radius1 and ay - radius1 < mouse[1] < ay + radius1:
+                planetName = optFont.render(name1, True, mercury)
+                gameDisplay.blit(planetName, [ax, ay + radius1 + 10])
+
         pygame.display.flip()
 
 
@@ -190,10 +224,10 @@ def start():
 
             mouse = pygame.mouse.get_pos()
 
-            if event.type == pygame.MOUSEBUTTONDOWN and 100 < mouse[0] < 190 and 530 < mouse[1] < 585:
+            if event.type == pygame.MOUSEBUTTONDOWN and 190 < mouse[0] < 340 and 530 < mouse[1] < 610:
                 play1()
 
-            if event.type == pygame.MOUSEBUTTONDOWN and 370 < mouse[0] < 500 and 255 < mouse[1] < 300:
+            if event.type == pygame.MOUSEBUTTONDOWN and 690 < mouse[0] < 1060 and 530 < mouse[1] < 600:
                 instrucions()
 
             if event.type == pygame.KEYDOWN:
@@ -204,18 +238,26 @@ def start():
 
         # Select font
         titleFont = pygame.font.SysFont('Calibri', 40, True, False)
-        buttonFont = pygame.font.SysFont('Calibri', 25, True, False)
+        buttonFont = pygame.font.Font('Montserrat.ttf', 60)
         title = titleFont.render("Welcome To The Solar System", True, pink)
-        play = buttonFont.render("Play", True, turquoise)
-        instruct = buttonFont.render("Instuctions", True, turquoise)
+
+        if 190 < mouse[0] < 340 and 530 < mouse[1] < 610:
+            play = buttonFont.render("Play", True, mercury)
+            instruct = buttonFont.render("Instructions", True, white)
+        elif 690 < mouse[0] < 1060 and 530 < mouse[1] < 600:
+            play = buttonFont.render("Play", True, white)
+            instruct = buttonFont.render("Instructions", True, mercury)
+        else:
+            play = buttonFont.render("Play", True, white)
+            instruct = buttonFont.render("Instructions", True, white)
 
         # Drawing code here
         gameDisplay.fill(black)
-        pygame.draw.rect(gameDisplay, white, [200, 530, 90, 55])
-        pygame.draw.rect(gameDisplay, white, [370, 255, 130, 45])
+        pygame.draw.rect(gameDisplay, black, [190, 530, 150, 80])
+        pygame.draw.rect(gameDisplay, black, [690, 530, 370, 70])
         gameDisplay.blit(title, [350, 100])
-        gameDisplay.blit(play, [220, 547])
-        gameDisplay.blit(instruct, [380, 270])
+        gameDisplay.blit(play, [200, 530])
+        gameDisplay.blit(instruct, [700, 530])
         pygame.display.flip()
 
     pygame.QUIT
