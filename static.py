ff225d4446d8993a82c09526442f44857fd4ad55
RhysH0w <93980599+RhysH0w@users.noreply.github.com>
2022-01-13 11:11:05 +0000

    Update static.py

--- static.py
+++ static.py
@@ -10,7 +10,11 @@ pink = (255, 14, 156)
 turquoise = (97, 231, 199)
 mercury = (131, 134, 139)
 
-size = (1640, 900)
+sizeInfo = pygame.display.Info()
+size = (sizeInfo.current_w, sizeInfo.current_h)
+
+centrex = size[0]/2
+centrey = size[1]/2
 
 infoFont = pygame.font.Font(os.path.join("static", "Slabo.ttf"), 50)
 infoTitle = pygame.font.Font(os.path.join("static", "Slabo.ttf"), 60)
