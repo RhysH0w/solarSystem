c793c4f7fd68aea8082f8f603cf4f419248d09c4
RhysH0w <93980599+RhysH0w@users.noreply.github.com>
2022-01-18 10:12:36 +0000

    Update static.py

--- static.py
+++ static.py
@@ -39,7 +39,3 @@ a = size[0] - 30
 b = size[0] - a
 
 hashedPass = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8" # This is a series of encoded characters that is used to compare to the encoded password
-
-
-
-
